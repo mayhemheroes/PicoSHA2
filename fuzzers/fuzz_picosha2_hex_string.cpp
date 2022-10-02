c38979938687cc617a2519faee52a1876b0ee187
baptistapedro <pedrojorgesbaptista@gmail.com>
2022-10-03 01:54:12 +0200

    Update fuzz_picosha2_hex_string.cpp

--- fuzzers/fuzz_picosha2_hex_string.cpp
+++ fuzzers/fuzz_picosha2_hex_string.cpp
@@ -2,14 +2,9 @@
 #include "picosha2.h"
 #include <string>
 
-void CalcAndOutput(const std::string& src){
-	std::cout << "src : \"" << src << "\"\n";
-	std::cout << "hash: " << picosha2::hash256_hex_string(src) << "\n" << std::endl;
-}
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
     std::string buf(reinterpret_cast<const char*>(data), size);
     buf.push_back('\0');
-    CalcAndOutput(buf.c_str());
+    picosha2::hash256_hex_string(buf.c_str());
     return 0;
 }
