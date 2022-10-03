842061043bc8472b545f4ff32af4fa42dfcf2024
baptistapedro <pedrojorgesbaptista@gmail.com>
2022-10-03 02:11:55 +0200

    Update fuzz_picosha2_hex_string.cpp

--- fuzzers/fuzz_picosha2_hex_string.cpp
+++ fuzzers/fuzz_picosha2_hex_string.cpp
@@ -5,6 +5,6 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
     std::string buf(reinterpret_cast<const char*>(data), size);
     buf.push_back('\0');
-    picosha2::hash256_hex_string(buf.c_str());
+    picosha2::hash256_hex_string(buf);
     return 0;
 }
